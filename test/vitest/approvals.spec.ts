b0f7ead5268ce4bb40c84c397ead47c00d98a615
Elliott Cuvelier <elliott.cuvelier@gmail.com>
2026-02-08 13:30:25 +0100

    refactor(test): write tests based on the golden master text test

--- test/vitest/approvals.spec.ts
+++ test/vitest/approvals.spec.ts
@@ -1,20 +1,40 @@
 import { GildedRose, Item } from "@/index";
+import { execSync } from "child_process";
 
-/**
- * This test uses Vitest Snapshot, similar to [Jest Snapshot](https://goo.gl/fbAQLP).
- *
- * There are two test cases here with different styles:
- * <li>"foo" is more similar to the unit test from the 'Java' version
- * <li>"thirtyDays" is more similar to the TextTest from the 'Java' version
- *
- * I suggest choosing one style to develop and deleting the other.
- */
+it("should match golden master for 30 days", () => {
+  const items = [
+    new Item("+5 Dexterity Vest", 10, 20),
+    new Item("Aged Brie", 2, 0),
+    new Item("Elixir of the Mongoose", 5, 7),
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  ];
+  const gildedRose = new GildedRose(items);
+  const snapshots: Array<
+    Array<{ name: string; sellIn: number; quality: number }>
+  > = [];
 
-describe("Gilded Rose Approval", () => {
-  it("should foo", () => {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
+  for (let day = 0; day < 30; day++) {
+    snapshots.push(
+      items.map((i) => ({
+        name: i.name,
+        sellIn: i.sellIn,
+        quality: i.quality,
+      })),
+    );
+    gildedRose.updateQuality();
+  }
 
-    expect(items).toMatchSnapshot();
+  expect(snapshots).toMatchSnapshot();
+});
+
+it("should thirtyDays", () => {
+  const consoleOutput = execSync("ts-node test/golden-master-text-test.ts 30", {
+    encoding: "utf-8",
   });
+
+  expect(consoleOutput).toMatchSnapshot();
 });
